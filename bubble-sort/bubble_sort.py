2eccd1776fa15898c82e9a7e834caa3969e74a92
Aidan Tiu <tiuprof77@gmail.com>
2024-04-29 11:20:28 +0800

    Add bubble sort exercises

--- bubble-sort/bubble_sort.py
+++ bubble-sort/bubble_sort.py
@@ -14,3 +14,31 @@ def bubble_sort(my_list):
 
 # Test
 print(bubble_sort([4, 3, 7, 1, 5]))
+
+def bubble_sort_2(my_list):
+    
+    # Define list length
+    list_length = len(my_list)
+
+    # check if the list is already sorted
+    is_sorted = False
+
+    while not is_sorted:
+
+        # set is_sorted to True
+        is_sorted = True
+
+        for i in range(list_length-1):
+
+            # if the element is greater than the next element
+            if my_list[i] > my_list[i+1]:
+
+                # swap the elements
+                my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+
+                # set is_sorted to False
+                is_sorted = False
+        
+        # decrement the list length
+        list_length -= 1
+    return my_list
